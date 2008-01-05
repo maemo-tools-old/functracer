9498a6a176e11d69d4e384118070c6bfd234240a
Anderson Lizardo <anderson.lizardo@indt.org.br>
2008-01-05 16:58:51 -0400

    Functracker: malloc_simple test improvement
    Improve malloc_simple test.
    
    Signed-off-by: Anderson Lizardo <anderson.lizardo@indt.org.br>

--- tests/malloc_simple.c
+++ tests/malloc_simple.c
@@ -3,12 +3,14 @@
 
 int main(void)
 {
-	char *x = NULL;
+	char *x = NULL, *y = NULL;
 
-	printf("before malloc(): x = %p\n", x);
-	x = malloc(100 * sizeof(char));
-	printf("after malloc(): x = %p\n", x);
+	printf("before malloc(): x = %p, y = %p\n", x, y);
+	x = malloc(123 * sizeof(char));
+	y = malloc(456 * sizeof(char));
+	printf("after malloc(): x = %p, y = %p\n", x, y);
 	free(x);
+	free(y);
 
 	return 0;
 }
