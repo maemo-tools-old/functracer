640029cc9f37e48e691c896ca2affde572dd0a1a
Anderson Lizardo <anderson.lizardo@indt.org.br>
2008-01-06 22:51:54 -0400

    Functracker: report module rewrite
    Rewrite report module (originally based on libleaks code). Now only the ll_copyfile() function was taken from libleaks code.
    
    Signed-off-by: Anderson Lizardo <anderson.lizardo@indt.org.br>

--- include/report.h
+++ include/report.h
@@ -1,52 +1,28 @@
-/* ========================================================================= *
- * General settings.
- * ========================================================================= */
+#include <stdio.h>
+#include <sys/types.h>
 
-#define TOOL_NAME       "functracker"
-#define TOOL_VERS       "0.7"
-#define TOOL_FILE       "%s/func-%d.%u.trace"
-#define TOOL_FILE_MAP   "%s/func-%d.%u.map"
-#define TOOL_SIGNAL     SIGUSR1
-#define TOOL_START      "PROFILE_START"
+#define MAX_NALLOC	(32 * 1024)	/* maximum number of allocations */
+#define MAX_BT_DEPTH	7		/* maximum backtrace depth */
 
-#define TOOL_LOGO       1
+struct rp_allocinfo {
+	struct rp_allocinfo *next;
+	void *addr;
+	size_t size;
+	void *backtrace[MAX_BT_DEPTH];
+	int bt_depth;
+};
 
-/* Define STACK_SIZE as 7 by default */
-#ifndef STACK_SIZE
-#define STACK_SIZE      7
-#endif
+struct bt_data;
 
-/* Define HEAP_SIZE as 32K simultaneous allocations */
-#ifndef HEAP_SIZE
-#define HEAP_SIZE       32
-#endif
+struct rp_data {
+	pid_t pid;
+	FILE *fp;
+	struct bt_data *btd;
+	struct rp_allocinfo *allocs;
+	long nallocs;
+};
 
-//#define DUMP_USING_INTERNAL_HEAP 1	/* doesn't work yet */
-
-/* ========================================================================= *
- * Definitions.
- * ========================================================================= */
-
-#define CAPACITY(a)        (sizeof(a) / sizeof(*a))
-
-/* Pointer to void* to use in allocations and call stack */
-typedef void* void_t;
-
-/* Structure for storing memory additional information about allocation point */
-struct SINFO;
-typedef struct SINFO INFO;
-
-struct SINFO
-{
-   INFO*    next;       /* Pointer to next struct in chunk   */
-   void_t   data;       /* Here is pointer to allocated data */
-   size_t   size;       /* Allocated block size in bytes     */
-   void_t   stack[STACK_SIZE]; /* Stack trace information    */
-}; /* struct SINFO */
-
-extern void ll_init(void);
-extern void ll_trace_signal(int signo);
-extern void* ll_calloc(size_t nmemb, size_t size);
-extern void* ll_malloc(size_t size);
-extern void* ll_realloc(void* ptr, size_t size);
-extern void ll_free(void* ptr);
+extern struct rp_data *rp_init(pid_t pid);
+extern void rp_new_alloc(struct rp_data *rd, void *addr, size_t size);
+extern void rp_dump(struct rp_data *rd);
+extern void rp_finish(struct rp_data *rd);
