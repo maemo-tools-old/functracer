30d3d26e93f87443b8077d0303aabf3042b03321
Bruna Moreira <bruna.moreira@indt.org.br>
2008-02-11 10:49:34 -0400

    Functracker: add copyright headers
    Add copyright headers.
    
    Signed-off-by: Bruna Moreira <bruna.moreira@indt.org.br>

--- include/dict.h
+++ include/dict.h
@@ -1,4 +1,26 @@
 /*
+ * This file is part of Functracker.
+ *
+ * Copyright (C) 1997-2007 Juan Cespedes <cespedes@debian.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ * Based on dictionary code from ltrace.
+ */
+
+/*
   Dictionary based on code by Morten Eriksen <mortene@sim.no>.
 */
 
--- src/report.c
+++ src/report.c
@@ -1,3 +1,22 @@
+/*
+ * This file is part of Functracker.
+ * 
+ * Copyright (C) 2008 by Nokia Corporation
+ * 
+ * Contact: Eero Tamminen <eero.tamminen@nokia.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 
+ * version 2 as published by the Free Software Foundation. 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Based on code from libleaks.
+ */
+
 #include <libiberty.h>
 #include <stdlib.h>
 #include <string.h>
