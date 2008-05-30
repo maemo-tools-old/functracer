050d122c51a664bda2f9edc485ac396c4fcfeffd
Ricardo Moraes <ricardo.guimaraes@indt.org.br>
2008-05-30 15:42:18 -0400

    Allow debug function to be disabled at compilation time Signed-off-by: Ricardo Moraes <ricardo.guimaraes@indt.org.br>

--- include/debug.h
+++ include/debug.h
@@ -26,7 +26,6 @@
 
 extern void debug_(int level, const char *file, int line, const char *func,
 	    const char *fmt, ...) __attribute__ ((format(printf, 5, 6)));
-#define msg_dbg(level, ...) debug_(level, __FILE__, __LINE__, __PRETTY_FUNCTION__, __VA_ARGS__)
 
 extern void msg_warn(const char *fmt, ...) __attribute__ ((format(printf, 1, 2)));
 extern void msg_err(const char *fmt, ...) __attribute__ ((format(printf, 1, 2)));
@@ -34,7 +33,12 @@ extern void error_file(const char *filename, const char *msg);
 
 /* XXX deprecated */
 #define error_exit(...)		msg_err(__VA_ARGS__)
-#define debug(level, ...)	msg_dbg(level, __VA_ARGS__)
+
+#ifdef DEBUG
+#define debug(level, ...)	debug_(level, __FILE__, __LINE__, __PRETTY_FUNCTION__, __VA_ARGS__)
+#else
+#define debug(level, ...)	do {} while (0)
+#endif
 
 #if __GNUC__ >= 3
 #define __unused	__attribute__ ((unused))
