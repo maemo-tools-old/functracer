d363970442fcf2f9657361eb803c3a0ae860d92a
Bruna Moreira <bruna.moreira@indt.org.br>
2008-12-19 13:32:59 -0400

    Functracer: add new tests for open/file operations
    This patch adds new tests for open/file operations. It will be used to verify
    file plugin.
    
    Signed-off-by: Bruna Moreira <bruna.moreira@indt.org.br>

---
+++ testsuite/functracer.file/open.c
@@ -0,0 +1,37 @@
+/*
+ * This file is part of Functracer.
+ *
+ * Copyright (C) 2008 by Nokia Corporation
+ *
+ * Contact: Eero Tamminen <eero.tamminen@nokia.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(void)
+{
+	char *path = "/etc/passwd";
+	int fd = open(path, O_RDONLY);
+
+	if (fd >= 0)
+		close(fd);
+	return 0;
+}
