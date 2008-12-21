1656b3e6756c29d4885791641776338525532b0f
Anderson Lizardo <anderson.lizardo@indt.org.br>
2008-12-20 21:00:10 -0400

    Functracer: add new test for posix_memalign and fix memalign test
    Add new test for posix_memalign() and fix memalign() test.
    
    Signed-off-by: Anderson Lizardo <anderson.lizardo@indt.org.br>

---
+++ testsuite/functracer.memory/posix_memalign.c
@@ -0,0 +1,38 @@
+/*
+ * This file is part of Functracer.
+ *
+ * Copyright (C) 2008 by Nokia Corporation
+ *
+ * Contact: Eero Tamminen <eero.tamminen@nokia.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int ret;
+	void *ptr = NULL;
+
+	printf("before posix_memalign(): ptr = %p\n", ptr);
+	ret = posix_memalign(&ptr, 4, 123 * sizeof(char));
+	printf("after posix_memalign(): ptr = %p, ret=%d\n", ptr, ret);
+	free(ptr);
+
+	return 0;
+}
