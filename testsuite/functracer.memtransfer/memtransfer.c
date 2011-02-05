06536eda0db7cc232db1ae2e6fcbfe8650e2439b
Andris Zeila <andris.zeila@accenture.com>
2011-02-05 20:48:48 +0200

    Added memtransfer module coverage testsuite.

---
+++ testsuite/functracer.memtransfer/memtransfer.c
@@ -0,0 +1,76 @@
+/*
+ * This file is part of Functracer.
+ *
+ * Copyright (C) 2011 by Nokia Corporation
+ *
+ * Contact: Eero Tamminen <eero.tamminen@nokia.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+
+#define _GNU_SOURCE
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <wchar.h>
+
+void test_char()
+{
+	char src[1024] = "source string", dst[1024], *ptr;
+	int len = strlen(src);
+
+	strcpy(dst, src);
+	memcpy(dst, src, len);
+	memmove(dst, src, len);
+	mempcpy(dst, src, len);
+	memset(dst, 0, 100);
+	strncpy(dst, src, 5);
+	stpcpy(dst, src);
+	strcat(dst, src);
+	strncat(dst, src, 5);
+	bcopy(src, dst, len);
+	bzero(dst, 100);
+	ptr = strdup(src);
+	free(ptr);
+	ptr = strndup(src, 5);
+	free(ptr);
+}
+
+void test_wchar()
+{
+	wchar_t src[1024] = L"source string", dst[1024], *ptr;
+	int len = wcslen(src);
+	wmemcpy(dst, src, len);
+	wmempcpy(dst, src, len);
+	wmemmove(dst, src, len);
+	wmemset(dst, 0, 100);
+	wcscpy(dst, src);
+	wcsncpy(dst, src, 5);
+	wcpcpy(dst, src);
+	wcpncpy(dst, src, 5);
+	wcscat(dst, src);
+	wcsncat(dst, src, 5);
+	ptr = wcsdup(src);
+	free(ptr);
+}
+int main(void)
+{
+	test_char();
+	test_wchar();
+	return 0;
+}
