b59e42f62734e73f617b3b8f2b2dbdf9af9a2f35
Andris Zeila <andris.zeila@accenture.com>
2011-02-07 12:34:38 +0200

    Audit module testsuite.

---
+++ testsuite/functracer.audit/audit.c
@@ -0,0 +1,37 @@
+/*
+ * This file is part of Functracer.
+ *
+ * Copyright (C) 2011 by Nokia Corporation
+ *
+ * Contact: Eero Tamminen <eero.tamminen@nokia.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void)
+{
+	char* ptr = malloc(100);
+	if (ptr == NULL) return -1;
+	ptr = realloc(ptr, 200);
+	if (ptr == NULL) return -1;
+	free(ptr);
+
+	return 0;
+}
