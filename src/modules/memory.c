05e6737bcc25658bfb9a61515e00b8d3716adcc9
Eero Tamminen <eero.tamminen@nokia.com>
2012-05-19 00:12:39 +0300

    reduce code by using common function
    Counter is now increased in common function used for trace output.
    
    Changed most frequently used functions to be checked first.

--- src/modules/memory.c
+++ src/modules/memory.c
@@ -4,7 +4,7 @@
  *
  * This file is part of Functracer.
  *
- * Copyright (C) 2008,2010 by Nokia Corporation
+ * Copyright (C) 2008-2012 by Nokia Corporation
  *
  * Contact: Eero Tamminen <eero.tamminen@nokia.com>
  *
@@ -52,174 +52,100 @@ static sp_rtrace_resource_t res_memory = {
 		.flags = SP_RTRACE_RESOURCE_DEFAULT,
 };
 
+static void write_function(struct process *proc, const char *name, unsigned int type, size_t size, pointer_t id)
+{
+	struct rp_data *rd = proc->rp_data;
+	sp_rtrace_fcall_t call = {
+		.type = type,
+		.index = rd->rp_number,
+		.context = context_mask,
+		.timestamp = RP_TIMESTAMP,
+		.name = name,
+		.res_size = size,
+		.res_id = id
+	};
+	sp_rtrace_print_call(rd->fp, &call);
+	rp_write_backtraces(proc, &call);
+	(rd->rp_number)++;
+}
 
 static void mem_function_exit(struct process *proc, const char *name)
 {
-	struct rp_data *rd = proc->rp_data;
-
 	addr_t retval = fn_return_value(proc);
 	size_t arg0 = fn_argument(proc, 0);
 
 	assert(proc->rp_data != NULL);
 	if (strcmp(name, "__libc_malloc") == 0) {
 		/* suppress allocation failures */
-		if (retval == 0) return;
-
-		sp_rtrace_fcall_t call = {
-				.type = SP_RTRACE_FTYPE_ALLOC,
-				.index = rd->rp_number,
-				.context = context_mask,
-				.timestamp = RP_TIMESTAMP,
-				.name = "malloc",
-				.res_size = arg0,
-				.res_id = (pointer_t)retval,
-		};
-		sp_rtrace_print_call(rd->fp, &call);
-		rp_write_backtraces(proc, &call);
-
+		if (retval) {
+			write_function(proc, "malloc", SP_RTRACE_FTYPE_ALLOC, arg0, retval);
+		}
+	} else if (strcmp(name, "__libc_free") == 0 ) {
+		/* Suppress "free(NULL)" calls from trace output.
+		 * They are a no-op according to ISO
+		 */
+		if (arg0) {
+			write_function(proc, "free", SP_RTRACE_FTYPE_FREE, 0, arg0);
+		}
 	} else if (strcmp(name, "__libc_calloc") == 0) {
-		size_t arg1;
 		/* suppress allocation failures */
-		if (retval == 0) return;
-		arg1 = fn_argument(proc, 1);
-
-		sp_rtrace_fcall_t call = {
-				.type = SP_RTRACE_FTYPE_ALLOC,
-				.index = rd->rp_number,
-				.context = context_mask,
-				.timestamp = RP_TIMESTAMP,
-				.name = "calloc",
-				.res_size = arg0 * arg1,
-				.res_id = (pointer_t)retval,
-		};
-		sp_rtrace_print_call(rd->fp, &call);
-		rp_write_backtraces(proc, &call);
-
-	} else if (strcmp(name, "__libc_memalign") == 0) {
-		size_t arg1;
-		/* suppress allocation failures */
-		if (retval == 0) return;
-		arg1 = fn_argument(proc, 1);
-
-		sp_rtrace_fcall_t call = {
-				.type = SP_RTRACE_FTYPE_ALLOC,
-				.index = rd->rp_number,
-				.context = context_mask,
-				.timestamp = RP_TIMESTAMP,
-				.name = "memalign",
-				.res_size = arg1,
-				.res_id = (pointer_t)retval,
-		};
-		sp_rtrace_print_call(rd->fp, &call);
-		rp_write_backtraces(proc, &call);
-
+		if (retval) {
+			size_t arg1 = fn_argument(proc, 1);
+			write_function(proc, "calloc", SP_RTRACE_FTYPE_ALLOC, arg0*arg1, retval);
+		}
 	} else if (strcmp(name, "__libc_realloc") == 0) {
 		size_t arg1 = fn_argument(proc, 1);
 		if (arg0 != 0) {
 			/* realloc acting normally (returning same or different
-			 * address) OR acting as free so showing the freeing */
-			sp_rtrace_fcall_t call = {
-					.type = SP_RTRACE_FTYPE_FREE,
-					.index = rd->rp_number++,
-					.context = context_mask,
-					.timestamp = RP_TIMESTAMP,
-					.name = "realloc",
-					.res_size = 0,
-					.res_id = (pointer_t)arg0,
-			};
-			sp_rtrace_print_call(rd->fp, &call);
-			rp_write_backtraces(proc, &call);
+			 * address) OR acting as free so showing the freeing
+			 */
+			write_function(proc, "realloc", SP_RTRACE_FTYPE_FREE, 0, arg0);
 		}
 		if (arg1 == 0 && retval == 0) {
 			/* realloc acting as free so return */
 			return;
 		}
-		/* show a new resource allocation (can be same or different
-		 * address)
+		/* show a new resource allocation
+		 * (can be same or different address)
 		 */
-		sp_rtrace_fcall_t call = {
-				.type = SP_RTRACE_FTYPE_ALLOC,
-				.index = rd->rp_number,
-				.context = context_mask,
-				.timestamp = RP_TIMESTAMP,
-				.name = "realloc",
-				.res_size = arg1,
-				.res_id = (pointer_t)retval,
-		};
-		sp_rtrace_print_call(rd->fp, &call);
-		rp_write_backtraces(proc, &call);
-
-	} else if (strcmp(name, "__libc_free") == 0 ) {
-		/* Suppress "free(NULL)" calls from trace output.
-		 * They are a no-op according to ISO
-		 */
-		if (arg0 == 0)
-			return;
-
-		sp_rtrace_fcall_t call = {
-				.type = SP_RTRACE_FTYPE_FREE,
-				.index = rd->rp_number,
-				.context = context_mask,
-				.timestamp = RP_TIMESTAMP,
-				.name = "free",
-				.res_size = 0,
-				.res_id = (pointer_t)arg0,
-		};
-		sp_rtrace_print_call(rd->fp, &call);
-		rp_write_backtraces(proc, &call);
+		write_function(proc, "realloc", SP_RTRACE_FTYPE_ALLOC, arg1, retval);
 
 	} else if (strcmp(name, "posix_memalign") == 0) {
-		size_t arg0 = fn_argument(proc, 0);
-		size_t arg2 = fn_argument(proc, 2);
-		if (retval != 0)
+		if (retval != 0) {
 			return;
-		/* posix_memalign() stores the allocated memory pointer on the
-		 * first argument (of type (void **)) */
+		}
+		/* posix_memalign() stores the allocated memory pointer on
+		 * the first argument (of type (void **))
+		 */
 		retval = trace_mem_readw(proc, arg0);
 		/* ignore allocation failures */
-		if (retval == 0) return;
-
-		sp_rtrace_fcall_t call = {
-				.type = SP_RTRACE_FTYPE_ALLOC,
-				.index = rd->rp_number,
-				.context = context_mask,
-				.timestamp = RP_TIMESTAMP,
-				.name = "posix_memalign",
-				.res_size = arg2,
-				.res_id = (pointer_t)retval,
-		};
-		sp_rtrace_print_call(rd->fp, &call);
-		rp_write_backtraces(proc, &call);
-
+		if (retval) {
+			size_t arg2 = fn_argument(proc, 2);
+			write_function(proc, "posix_memalign", SP_RTRACE_FTYPE_ALLOC, arg2, retval);
+		}
+	} else if (strcmp(name, "__libc_memalign") == 0) {
+		/* suppress allocation failures */
+		if (retval) {
+			size_t arg1 = fn_argument(proc, 1);
+			write_function(proc, "memalign", SP_RTRACE_FTYPE_ALLOC, arg1, retval);
+		}
 	} else if (strcmp(name, "__libc_valloc") == 0 || strcmp(name, "valloc") == 0) {
 		/* suppress allocation failures */
-		if (retval == 0) return;
-		sp_rtrace_fcall_t call = {
-				.type = SP_RTRACE_FTYPE_ALLOC,
-				.index = rd->rp_number,
-				.context = context_mask,
-				.timestamp = RP_TIMESTAMP,
-				.name = "valloc",
-				.res_size = arg0,
-				.res_id = (pointer_t)retval,
-		};
-		sp_rtrace_print_call(rd->fp, &call);
-		rp_write_backtraces(proc, &call);
-	}
-	else {
+		if (retval) {
+			write_function(proc, "valloc", SP_RTRACE_FTYPE_ALLOC, arg0, retval);
+		}
+	} else {
 		msg_warn("unexpected function exit (%s)\n", name);
-		return;
 	}
-	(rd->rp_number)++;
 }
 
 static struct plg_symbol symbols[] = {
-		{.name = "__libc_calloc", .hit = 0},
 		{.name = "__libc_malloc", .hit = 0},
-		{.name = "__libc_realloc", .hit = 0},
 		{.name = "__libc_free", .hit = 0},
-		{.name = "__libc_memalign", .hit = 0},
+		{.name = "__libc_calloc", .hit = 0},
+		{.name = "__libc_realloc", .hit = 0},
 		{.name = "posix_memalign", .hit = 0},
+		{.name = "__libc_memalign", .hit = 0},
 		{.name = "valloc", .hit = 0},
 };
 
